15c6137183bb85156132c1ca21a0351a90d515de
15533 <too_egg@yahoo.co.jp>
2019-10-16 05:23:35 +0000

    learn js

--- kadai-2/index.js
+++ kadai-2/index.js
@@ -1,2 +1,34 @@
+const Ben =　'Adam'
+const Jack = 'Frank'
+console.log(Ben)
+console.log(Jack)
+
+let number = 3
+number = number + 1
+console.log(number)
+
+let Alex = 'Alex2'
+Alex = 'Alex3'
+console.log(Alex)
+
+const isEgg = true
+console.log(isEgg)
+
+const phrase = "I'm an Egg."
+console.log(phrase)
+const phrase2 = 'He said,"I am an Egg.".'
+console.log(phrase2)
+const pharse3 = 'I\'m a Cat.'
+console.log(pharse3)
+const pharse4 = "He said,\"I am an Egg.\"."
+console.log(pharse4)
+
+const word = " shit"
+console.log(typeof word)
+const age = 13
+console.log(typeof age)
+const Me = false
+console.log(typeof Me)
+
 console.log('abcd')
 console.log('abcd')
