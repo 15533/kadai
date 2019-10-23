f19474e15cbb4db1afab9034fe56a7d2d3e36303
15533 <too_egg@yahoo.co.jp>
2019-10-23 04:53:39 +0000

    add flag

---
+++ kadai-3/index.js
@@ -0,0 +1,11 @@
+const firstname = 'iron'
+const familyname = 'man'
+console.log(firstname + familyname)
+
+console.log(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)
+
+const dog = false
+const cat = false
+console.log(dog || cat)
+console.log(dog && cat)
+console.log(!dog && !cat)
